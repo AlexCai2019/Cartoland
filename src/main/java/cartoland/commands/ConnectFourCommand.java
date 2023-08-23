79028ad5a619ebb8fc653928fbb98b34ef962b2f
brick-bk <137702200+brick-bk@users.noreply.github.com>
2023-08-23 19:43:41 +0800

    Update ConnectFourCommand.java 20230823(2)

--- src/main/java/cartoland/commands/ConnectFourCommand.java
+++ src/main/java/cartoland/commands/ConnectFourCommand.java
@@ -87,19 +87,19 @@ public class ConnectFourCommand extends HasSubcommands
 			int column = columnBox - 1; //拆箱 因為columnBox是以1為開始 所以要 - 1
 			if (!connectFour.isInBounds(column))
 			{
-				event.reply("connect_four.must_be_in_range").setEphemeral(true).queue();
+				event.reply(userID, "connect_four.must_be_in_range").setEphemeral(true).queue();
 				return;
 			}
 
 			if (connectFour.isFull(column)) //直行已經滿了卻還是放棋子
 			{
-				event.reply("connect_four.can_t_put").setEphemeral(true).queue();
+				event.reply(userID, "connect_four.can_t_put").setEphemeral(true).queue();
 				return;
 			}
 
 			if (connectFour.humanPlace(column)) //如果玩家贏了
 			{
-				event.reply("connect_four.win" + connectFour.getBoard()).queue();
+				event.reply(userID, "connect_four.win" + connectFour.getBoard()).queue();
 				games.remove(userID);
 				return;
 			}
@@ -108,7 +108,7 @@ public class ConnectFourCommand extends HasSubcommands
 
 			if (connectFour.aiPlace()) //如果機器人贏了
 			{
-				event.reply("connect_four.lose" + connectFour.getBoard()).queue();
+				event.reply(userID, "connect_four.lose" + connectFour.getBoard()).queue();
 				games.remove(userID);
 				return;
 			}
@@ -116,12 +116,12 @@ public class ConnectFourCommand extends HasSubcommands
 			if (connectFour.isTie()) //如果平手
 			{
 				//之所以不像井字遊戲那樣在機器人動之前執行 是因為這個棋盤有偶數個格子 因此最後一步一定是機器人來下
-				event.reply("connect_four.tie" + connectFour.getBoard()).queue();
+				event.reply(userID, "connect_four.tie" + connectFour.getBoard()).queue();
 				games.remove(userID);
 				return;
 			}
 
-			event.reply("connect_four.your_move" + playerMove + "\nconnect_four.bot_s_move" + connectFour.getBoard() + "\n</connect_four play:1142380307509690458>").setEphemeral(true).queue();
+			event.reply((userID, "connect_four.your_move") + playerMove + "\n" + (userID, "connect_four.bot_s_move") + connectFour.getBoard() + "\n</connect_four play:1142380307509690458>").setEphemeral(true).queue();
 		}
 	}
 }
